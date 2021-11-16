a867f84b62ad20ff64fed582a95bf14ed0c4221e
Daniel Cohen Gindi <danielgindi@gmail.com>
2021-11-16 10:47:12 +0200

    Support for VS 2022

--- utils/msbuild.js
+++ utils/msbuild.js
@@ -10,8 +10,8 @@ class MsbuildUtil {
         for (let programFiles of [
             process.env['ProgramFiles'],
             process.env['ProgramFiles(x86)']].filter(x => x)) {
-            for (let versionCode of ['Current', '15.0', '14.0']) {
-                for (let versionYear of ['2017', '2019']) {
+            for (let versionCode of ['Current', '17.0', '15.0', '14.0']) {
+                for (let versionYear of ['2017', '2019', '2022']) {
                     options.push(`${programFiles}\\Microsoft Visual Studio\\${versionYear}\\Community\\MSBuild\\${versionCode}\\Bin\\MSBuild.exe`);
                 }
             }
@@ -32,7 +32,7 @@ class MsbuildUtil {
         for (let programFiles of [
             process.env['ProgramFiles'],
             process.env['ProgramFiles(x86)']].filter(x => x)) {
-            for (let versionYear of ['2017', '2019']) {
+            for (let versionYear of ['2017', '2019', '2022']) {
                 options.push(`${programFiles}\\Microsoft Visual Studio\\${versionYear}\\Community\\Common7\\IDE\\devenv.exe`);
             }
         }
